233a3879d1cfe2144e85fa22fa0b9a972f19074e
Jeremy Ruby <JeremyeRuby@gmail.com>
2020-08-30 09:49:36 -0400

    uses += operator

---
+++ basic_javascript_3.js
@@ -0,0 +1,5 @@
+function myfunction() { //onclick uses myfunction
+    var sentence = "I am learning";//displays this and the next line 
+    sentence +="a lot from this book!";
+    document.getElementById("concatenate").innerHTML = sentence;//the id is concatenate
+  }
