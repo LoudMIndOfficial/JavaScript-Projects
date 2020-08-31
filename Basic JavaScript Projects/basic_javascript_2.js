de6ccc230d7dcb96748ec7c81c2d08028a5ac64c
Jeremy Ruby <JeremyeRuby@gmail.com>
2020-08-31 05:05:38 -0400

    wrong folder location

--- Basic JavaScript Projects/basic_javascript_2.js
+++
@@ -1,5 +0,0 @@
-function example() { //Button Switches names ...expression
-    var str = document.getElementById("cat").innerHTML; 
-    var txt = str.replace("Yes","No");
-    document.getElementById("cat").innerHTML = txt;
-  }
